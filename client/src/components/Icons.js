f5eea06db1a9d3254f1dff46285761991d206adb
Benson Ntemo <ntemobbenbast@gmail.com>
2025-12-05 14:48:38 +0100

    Icons changed

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 export const QuestionIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-cosbia-accent">
     <circle cx="12" cy="12" r="10"/>
